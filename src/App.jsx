3e450418ced09bceba1aa2c48ada8e41e4e95dd4
ancung <ancungaulia@gmail.com>
2025-08-09 15:39:50 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1>Wangsit</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
